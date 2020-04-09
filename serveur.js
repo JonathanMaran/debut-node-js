5cde206fec13301206118b93b440fe9654b0ba6a
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2020-04-09 11:21:22 +0200

    Insertion de conditions pour déterminer sur quelle page je suis et quoi afficher

--- serveur.js
+++ serveur.js
@@ -1,22 +1,18 @@
 let http = require('http'); //require effectue un appel à une bibliothèque de Node.js, ici la bibliothèque "http" qui nous permet de créer un serveur web.
-// let url = require("url");
+let url = require('url');
 
-let server = http.createServer(function(req, res) { //La variable http représente un objet JavaScript qui va nous permettre de lancer un serveur web.
-    // let page = url.parse(req.url).pathname;
-    // console.log(page);
-    res.writeHead(200, {"Content-Type": "text/html"}); //code 200 dans l'en-tête de la réponse, qui signifie au navigateur "OK tout va bien", + Type MIME
-    res.write('<!DOCTYPE html>'+
-        '<html>'+
-        '    <head>'+
-        '        <meta charset="utf-8" />'+
-        '        <title>Ma page Node.js !</title>'+
-        '    </head>'+
-        '    <body>'+
-        '     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
-        '    </body>'+
-        '</html>');
-    res.end();
-    res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>'); //on termine la réponse (avecend()) en envoyant le message de notre choix au navigateur
+let server = http.createServer(function (req, res) { //La variable http représente un objet JavaScript qui va nous permettre de lancer un serveur web.
+    let page = url.parse(req.url).pathname;
+    console.log(page);
+    res.writeHead(200, {"Content-Type": "text/plain"}); //code 200 dans l'en-tête de la réponse, qui signifie au navigateur "OK tout va bien", + Type MIME
+    if (page == '/') {
+        res.write('Vous etes sur la page d\'accueil');
+    } else if (page == '/gallery') {
+        res.write('Bienvenue sur la page Galerie!')
+    } else  if (page == '/game') {
+        res.write('Faites vos jeux!')
+    }
+    res.end(); //on termine la réponse (avecend()) en envoyant le message de notre choix au navigateur
 });
 server.listen(8080); //le serveur est lancé et "écoute" sur le port 8080
 
