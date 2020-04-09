9b3c46f425d4c5ce133ef640b133de9f8e2ffd8b
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2020-04-09 11:08:53 +0200

    test écriture HTML grâce au Type MIME

--- serveur.js
+++ serveur.js
@@ -1,8 +1,22 @@
-var http = require('http'); //require effectue un appel à une bibliothèque de Node.js, ici la bibliothèque "http" qui nous permet de créer un serveur web.
+let http = require('http'); //require effectue un appel à une bibliothèque de Node.js, ici la bibliothèque "http" qui nous permet de créer un serveur web.
+// let url = require("url");
 
-var server = http.createServer(function(req, res) { //La variable http représente un objet JavaScript qui va nous permettre de lancer un serveur web.
-    res.writeHead(200); //code 200 dans l'en-tête de la réponse, qui signifie au navigateur "OK tout va bien"
-    res.end('Salut tout le monde !'); //on termine la réponse (avecend()) en envoyant le message de notre choix au navigateur
+let server = http.createServer(function(req, res) { //La variable http représente un objet JavaScript qui va nous permettre de lancer un serveur web.
+    // let page = url.parse(req.url).pathname;
+    // console.log(page);
+    res.writeHead(200, {"Content-Type": "text/html"}); //code 200 dans l'en-tête de la réponse, qui signifie au navigateur "OK tout va bien", + Type MIME
+    res.write('<!DOCTYPE html>'+
+        '<html>'+
+        '    <head>'+
+        '        <meta charset="utf-8" />'+
+        '        <title>Ma page Node.js !</title>'+
+        '    </head>'+
+        '    <body>'+
+        '     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
+        '    </body>'+
+        '</html>');
+    res.end();
+    res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>'); //on termine la réponse (avecend()) en envoyant le message de notre choix au navigateur
 });
 server.listen(8080); //le serveur est lancé et "écoute" sur le port 8080
 
